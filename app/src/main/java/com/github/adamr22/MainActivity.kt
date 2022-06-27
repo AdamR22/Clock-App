4641d5f35fd5aa9a5eec818626eef4feb9ec8afb
Adam Rao <raoadam87@gmail.com>
2022-06-27 17:36:00 +0300

    Finishes bottom nav bar

--- app/src/main/java/com/github/adamr22/MainActivity.kt
+++ app/src/main/java/com/github/adamr22/MainActivity.kt
@@ -2,10 +2,50 @@ package com.github.adamr22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.adamr22.alarm.AlarmFragment
+import com.github.adamr22.bedtime.BedTimeFragment
+import com.github.adamr22.clock.ClockFragment
+import com.github.adamr22.stopwatch.StopWatchFragment
+import com.github.adamr22.timer.TimerFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNav)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.alarm -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, AlarmFragment.newInstance()).commit()
+                    true
+                }
+                R.id.clock -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ClockFragment.newInstance()).commit()
+                    true
+                }
+                R.id.timer -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, TimerFragment.newInstance()).commit()
+                    true
+                }
+                R.id.stop_watch -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, StopWatchFragment.newInstance()).commit()
+                    true
+                }
+                R.id.bed_time -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, BedTimeFragment.newInstance()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
