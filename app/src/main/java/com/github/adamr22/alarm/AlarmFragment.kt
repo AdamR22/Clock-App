73083832ab451e171e2e965127903fdb49a80910
Adam Rao <raoadam87@gmail.com>
2022-06-30 18:10:05 +0300

    Creates time picker upon add alarm button

--- app/src/main/java/com/github/adamr22/alarm/AlarmFragment.kt
+++ app/src/main/java/com/github/adamr22/alarm/AlarmFragment.kt
@@ -1,16 +1,22 @@
 package com.github.adamr22.alarm
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.adamr22.R
+import com.github.adamr22.common.TimePicker
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.timepicker.MaterialTimePicker
 
 class AlarmFragment : Fragment() {
     
     lateinit var addAlarmButton: FloatingActionButton
+    lateinit var picker: MaterialTimePicker
+
+    private val TAG = "AlarmFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +33,19 @@ class AlarmFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addAlarmButton = view.findViewById(R.id.fab_add)
+        picker = TimePicker.buildPicker("Set Alarm")
     }
 
     override fun onResume() {
         super.onResume()
-        // TODO: Add Alarm functionality here 
+        // TODO: Add Alarm functionality here
+        addAlarmButton.setOnClickListener {
+            picker.show(parentFragmentManager, "Alarm Picker")
+
+            picker.addOnPositiveButtonClickListener {
+                Log.d(TAG, "onResume: ${picker.hour}:${picker.minute}")
+            }
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/github/adamr22/common/TimePicker.kt
@@ -0,0 +1,14 @@
+package com.github.adamr22.common
+
+import com.github.adamr22.R
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+
+object TimePicker {
+
+
+    fun buildPicker(titleText: String): MaterialTimePicker {
+        return MaterialTimePicker.Builder().setTheme(R.style.TimePickerLight)
+            .setTimeFormat(TimeFormat.CLOCK_24H).setTitleText(titleText).build()
+    }
+}
