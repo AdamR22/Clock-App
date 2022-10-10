303e0ca33c5502c6ac155f19006a4683f863904e
Adam Rao <raoadam87@gmail.com>
2022-10-10 21:24:10 +0300

    Finishes stopwatch UI

--- app/src/main/java/com/github/adamr22/stopwatch/StopWatchFragment.kt
+++ app/src/main/java/com/github/adamr22/stopwatch/StopWatchFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import com.github.adamr22.R
 
 class StopWatchFragment : Fragment() {
@@ -15,6 +18,7 @@ class StopWatchFragment : Fragment() {
     }
 
     private lateinit var viewModel: StopWatchViewModel
+    private val exampleList = arrayOf("Hey", "Hello", "Niaje", "Rada", "Irio nade", "Ilal Mang'eny")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,24 @@ class StopWatchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (requireContext() as AppCompatActivity).supportActionBar?.title = "Stopwatch"
+
         return inflater.inflate(R.layout.fragment_stop_watch, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val listView: ListView = view.findViewById(R.id.lap_list)
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.lap_item_layout,
+            R.id.tv_lap_time,
+            exampleList
+        )
+
+        listView.adapter = adapter
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
