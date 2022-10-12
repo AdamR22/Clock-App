442df38703bfee3d7028bfd3e6a406b3a328b814
Adam Rao <raoadam87@gmail.com>
2022-10-12 19:41:04 +0300

    Programs stopwatch states

--- app/src/main/java/com/github/adamr22/stopwatch/StopWatchFragment.kt
+++ app/src/main/java/com/github/adamr22/stopwatch/StopWatchFragment.kt
@@ -2,27 +2,48 @@ package com.github.adamr22.stopwatch
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.github.adamr22.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.flow.collectLatest
 
 class StopWatchFragment : Fragment() {
+
+    private val TAG = "StopWatchFragment"
 
     companion object {
         fun newInstance() = StopWatchFragment()
     }
 
+    private lateinit var btnReset: FloatingActionButton
+    private lateinit var btnPause: ImageButton
+    private lateinit var btnPlay: FloatingActionButton
+    private lateinit var btnAddLap: FloatingActionButton
+
+    private lateinit var stopwatchTime: TextView
+
     private lateinit var viewModel: StopWatchViewModel
-    private val exampleList = arrayOf("Hey", "Hello", "Niaje", "Rada", "Irio nade", "Ilal Mang'eny")
+    private lateinit var lapTimeLists: ListView
+
+
+    private val timeStamps = arrayOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StopWatchViewModel::class.java)
+        viewModel = ViewModelProvider(this)[StopWatchViewModel::class.java]
+        viewModel.changeState(viewModel.initialOrdinalValue)
+
+        Log.d(TAG, "onCreate: OnCreate Called")
     }
 
     override fun onCreateView(
@@ -35,18 +56,68 @@ class StopWatchFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val listView: ListView = view.findViewById(R.id.lap_list)
+        btnReset = view.findViewById(R.id.btn_reset_stopwatch)
+        btnAddLap = view.findViewById(R.id.btn_add_lap)
+        btnPlay = view.findViewById(R.id.play)
+        btnPause = view.findViewById(R.id.btn_pause)
+
+        stopwatchTime = view.findViewById(R.id.stopwatch_content)
+
+        lapTimeLists = view.findViewById(R.id.lap_list)
 
         val adapter = ArrayAdapter(
             requireContext(),
             R.layout.lap_item_layout,
             R.id.tv_lap_time,
-            exampleList
+            timeStamps
         )
 
-        listView.adapter = adapter
+        lapTimeLists.adapter = adapter
 
         super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onResume() {
+
+        lapTimeLists.visibility = if (timeStamps.isEmpty()) View.GONE else View.VISIBLE
+
+        btnPause.setOnClickListener {
+            viewModel.changeState(viewModel.pauseOrdinalValue)
+        }
+
+        btnPlay.setOnClickListener {
+            viewModel.changeState(viewModel.playOrdinalValue)
+        }
+
+        renderUI()
+
+        super.onResume()
+    }
+
+    private fun renderUI() {
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            viewModel.stopwatchState.collectLatest {
+                when (it) {
+                    StopWatchViewModel.StopWatchStates.INITIAL -> {
+                        btnPause.visibility = View.GONE
+                        btnAddLap.visibility = View.GONE
+                        btnReset.visibility = View.GONE
+                        btnPlay.visibility = View.VISIBLE
+                    }
+                    StopWatchViewModel.StopWatchStates.PAUSED -> {
+                        btnPause.visibility = View.GONE
+                        btnAddLap.visibility = View.GONE
+                        btnReset.visibility = View.VISIBLE
+                        btnPlay.visibility = View.VISIBLE
+                    }
+                    StopWatchViewModel.StopWatchStates.RUNNING -> {
+                        btnPause.visibility = View.VISIBLE
+                        btnAddLap.visibility = View.VISIBLE
+                        btnReset.visibility = View.VISIBLE
+                        btnPlay.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/github/adamr22/stopwatch/StopWatchViewModel.kt
+++ app/src/main/java/com/github/adamr22/stopwatch/StopWatchViewModel.kt
@@ -1,7 +1,24 @@
 package com.github.adamr22.stopwatch
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class StopWatchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val initialOrdinalValue = 0
+    val pauseOrdinalValue = 1
+    val playOrdinalValue = 2
+
+    enum class StopWatchStates {
+        INITIAL,
+        PAUSED,
+        RUNNING,
+    }
+
+    private var _stopwatchState = MutableStateFlow(StopWatchStates.INITIAL)
+    val stopwatchState: StateFlow<StopWatchStates> = _stopwatchState
+
+    fun changeState(ordinalValue: Int) {
+        _stopwatchState.value = StopWatchStates.values()[ordinalValue]
+    }
 }
