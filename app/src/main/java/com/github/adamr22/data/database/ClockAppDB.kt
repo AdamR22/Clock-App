afe2d4da8842213a17add3ad04ba5188a13b6464
Adam Rao <raoadam87@gmail.com>
2022-10-21 18:56:14 +0300

    Creates database for app

---
+++ app/src/main/java/com/github/adamr22/data/database/ClockAppDB.kt
@@ -0,0 +1,35 @@
+package com.github.adamr22.data.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.github.adamr22.data.schemas.AlarmContract
+import com.github.adamr22.data.schemas.ScheduleContract
+
+class ClockAppDB(
+    context: Context?,
+) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "ClockApp.db"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.let {
+            it.setForeignKeyConstraintsEnabled(true)
+            it.execSQL(AlarmContract.SQL_CREATE_ALARM_ENTRIES)
+            it.execSQL(ScheduleContract.SQL_CREATE_SCHEDULE_ENTRIES)
+            onCreate(it)
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.let {
+            it.setForeignKeyConstraintsEnabled(false)
+            it.execSQL(AlarmContract.SQL_DELETE_ALARM_ENTRIES)
+            it.execSQL(ScheduleContract.SQL_DELETE_SCHEDULE_ENTRIES)
+            onCreate(it)
+        }
+    }
+}
